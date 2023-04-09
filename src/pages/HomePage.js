a42bf42868644a8c3a20f854d7cecf648b99cbb4
Seung-hwan285 <ghks123285@gmail.com>
2023-04-09 09:22:42 +0900

    refactor: Change naming

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,18 +1,18 @@
 function HomePage(target){
 
-    const homeContainer=document.createElement('div');
-    homeContainer.className='Banner__Container';
+    const HomeContainer=document.createElement('div');
+    HomeContainer.className='Banner__Container';
     const container = document.querySelector('.Banner__Wrapper');
 
     if(container){
         return;
     }
 
-    const homeWrapper = document.createElement('div');
-    homeWrapper.className='Banner__Wrapper';
+    const HomeWrapper = document.createElement('div');
+    HomeWrapper.className='Banner__Wrapper';
 
-    homeContainer.appendChild(homeWrapper);
-    target.appendChild(homeContainer);
+    HomeContainer.appendChild(HomeWrapper);
+    target.appendChild(HomeContainer);
 
 
 
@@ -22,7 +22,7 @@ function HomePage(target){
             Login.remove();
         }
 
-        homeWrapper.innerHTML=`
+        HomeWrapper.innerHTML=`
             <h1 class="banner-title">conduit</h1>
             <p class="banner-content">A place to share your hnwledge.</p>
         `
