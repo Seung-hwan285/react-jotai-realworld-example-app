0e325729d454c47c51ec6b1a481a1df2398dab16
Seung-hwan285 <ghks123285@gmail.com>
2023-04-19 21:13:26 +0900

    feat: TagList loading spinner

--- src/components/Home/HomeTagList.js
+++ src/components/Home/HomeTagList.js
@@ -1,10 +1,24 @@
 import { tag_request } from '../../lib/tag/request.js';
 
 function HomeTagList(row) {
+  const col = document.createElement('div');
+  col.className = 'col-md-3';
+
+  col.innerHTML = /* HTML */ `
+    <div class="sidebar">
+      <p>Popular Tags</p>
+      <div class="tag-list">
+        <div class="spinner-container">
+          <div class="spinner"></div>
+        </div>
+      </div>
+    </div>
+  `;
+
+  row.appendChild(col);
+
   const render = async () => {
     const { tags } = await tag_request.getTagsList();
-    const col = document.createElement('div');
-    col.className = 'col-md-3';
 
     col.innerHTML = `
      <div class="sidebar">
