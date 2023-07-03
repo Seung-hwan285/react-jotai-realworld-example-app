365c4b2f2ceb770cf9c1d0c9f41b90c6c54dfaa4
Seung-hwan285 <ghks123285@gmail.com>
2023-07-03 20:07:10 +0900

    chore: Update file

--- dist.js/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var _App = _interopRequireDefault(require("./App.js"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-const app = document.querySelector('.app');
-new _App.default({
-  target: app
-});
--- dist.js/url.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.API_END_POINT = void 0;
-const API_END_POINT = 'https://api.realworld.io';
-exports.API_END_POINT = API_END_POINT;
