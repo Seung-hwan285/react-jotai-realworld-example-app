a1a777ab8d1991e6905c0db3cbff3fc0ff512e31
Seung-hwan285 <ghks123285@gmail.com>
2023-07-03 16:21:34 +0900

    chore: test

--- src/components/Home/HomeBanner.js
+++ src/components/Home/HomeBanner.js
@@ -1,6 +1,7 @@
 function HomeBanner() {
   const bannerWrapper = document.querySelector('.banner .container');
 
+  // test
   return (bannerWrapper.innerHTML = /* HTML */ `
     <h1 class="logo-font">conduit</h1>
     <p>A place to share your knowledge.</p>
